953ae28fcfbc0b31425ce90ff9eba35593c9e214
WiseScholar <ebenezertepe209@gmail.com>
2024-08-05 21:09:27 +0000

    made some changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleLED(state) {
-    fetch('http://192.168.77.68/LED/' + state)
+    fetch('http://192.168.77.69/LED/' + state) // Update with your ESP8266's IP address
         .then(response => response.text())
         .then(data => {
             console.log(data);
@@ -7,4 +7,4 @@ function toggleLED(state) {
         .catch(error => {
             console.error('Error:', error);
         });
-  }
+}
