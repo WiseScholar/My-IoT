90d239cf618249a03bb307f8db62b68b63ab5640
WiseScholar <ebenezertepe209@gmail.com>
2024-08-05 22:55:48 +0000

    back to normal

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function toggleLED(state) {
-    fetch('http://192.168.77.68/LED/' + state)
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    fetch('/LED/' + state)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
