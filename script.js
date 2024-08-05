7b807afbc47e2e5128e3d91eab1f9134a1a065c3
WiseScholar <ebenezertepe209@gmail.com>
2024-08-05 22:39:22 +0000

    new try

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function toggleLED(state) {
-    fetch('/LED/' + state)
-        .then(response => response.text())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    fetch('http://192.168.77.68/LED/' + state)
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
